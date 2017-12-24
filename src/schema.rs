372bdc1b102c65d083f2621742c32a4fc3fa4c47
Michael Fletcher <m.fletcher@theplanet.ca>
2017-12-24 13:08:01 -0700

    Added schema so it builds without a database.

--- src/schema.rs
+++ src/schema.rs
@@ -1,1 +1,9 @@
-infer_schema!("dotenv:DATABASE_URL");
+table! {
+    birthdays (id) {
+        id -> Int4,
+        title -> Varchar,
+        year -> Nullable<Int4>,
+        month -> Int4,
+        day -> Int4,
+    }
+}
