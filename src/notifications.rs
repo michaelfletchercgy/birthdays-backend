65645a6bb43b89f7d6b8abc4d7b5601bd4c00b65
Michael Fletcher <m.fletcher@theplanet.ca>
2018-06-26 20:23:00 -0600

    More messages.

--- src/notifications.rs
+++ src/notifications.rs
@@ -15,7 +15,7 @@ pub fn push_to_subscriptions(con:&PgConnection) {
     // filter for unique user ids with birthdays in the next seven days.  Gotta figure out a weird OR clause to do that.
 
     println!("Sending notifications.");
-    
+
     let users_with_nots = birthdays
         .select(schema::birthdays::dsl::user_id)
         .filter(
@@ -29,16 +29,22 @@ pub fn push_to_subscriptions(con:&PgConnection) {
         .load::<i32>(con)
         .expect("error loading birthdays");
 
+    println!("Users with notifications: {}", users_with_nots.len());
+
     let client = reqwest::Client::new();
 
     for user_id in users_with_nots {
+        println!("Sending notifications for {}", user_id);
+
         let subs = subscriptions
             .select(url)
             .filter(schema::subscriptions::dsl::user_id.eq(user_id))
             .load::<String>(con)
             .expect("error");
 
+        println!("{} has {} subscriptions", user_id, subs.len());
         for u in subs {
+            println!("Posting to {}", u);
             client.post(&u)
                 .body("")
                 .send().unwrap();
